a538dc397db07cb21901250b94b43f51e9461747
Sim <v_sim@wincoll.ac.uk>
2021-08-20 13:30:16 +0800

    Reconvert to categircal

--- v3.0.0/q_network.py
+++ v3.0.0/q_network.py
@@ -3,10 +3,11 @@ from keras.layers import Input
 from keras.layers import Activation
 from keras.layers import Conv2D, Dense
 from keras.layers import add, BatchNormalization, Flatten
-from keras.losses import SparseCategoricalCrossentropy,BinaryCrossentropy
+from keras.losses import CategoricalCrossentropy,BinaryCrossentropy
 import tensorflow as tf
 from board_conversion import *
 from variable_settings import *
+from keras.utils.generic_utils import get_custom_objects
 class Q_model():
     def __init__(self,model = None):
         if model:
@@ -19,25 +20,24 @@ class Q_model():
         def residual_module(layer_in, n_filters):
             merge_input = layer_in
             if layer_in.shape[-1] != n_filters:
-                merge_input = Conv2D(n_filters, (1,1), padding='same', activation='relu', kernel_initializer='he_normal')(layer_in)
-            conv1 = Conv2D(n_filters, (3,3), padding='same', activation='relu', kernel_initializer='he_normal')(layer_in)
+                merge_input = Conv2D(n_filters, (1,1), padding='same', activation='relu')(layer_in)
+            conv1 = Conv2D(n_filters, (3,3), padding='same', activation='relu')(layer_in)
             batch_norm = BatchNormalization()(conv1)
             layer_out = add([batch_norm, merge_input])
             layer_out = Activation('relu')(layer_out)
             return layer_out
 
-        visible = Input(shape=(17, 8, 8,12))
+        visible = Input(shape=(17,8, 8,12))
         layer1 = residual_module(visible, 64)
-        layer2 = residual_module(layer1, 64)
-        layer3 = residual_module(layer2, 64)
+        layer2 = residual_module(layer1, 128)
+        layer3 = residual_module(layer2, 256)
         flatten = Flatten()(layer3)
         p = Dense(4096,activation='softmax', name = 'p')(flatten)
-        # ? p = filter_legal_moves()
         v = Dense(1,activation = 'sigmoid', name = 'v')(flatten)
         
         model = Model(inputs=visible, outputs=[p,v])
         
-        model.compile(optimizer = optimizer, loss = {'p':SparseCategoricalCrossentropy(),
+        model.compile(optimizer = optimizer, loss = {'p':CategoricalCrossentropy(),
                                                      'v':BinaryCrossentropy()})
         return model
     
