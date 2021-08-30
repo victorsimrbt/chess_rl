2adf77b364770d4ebcc29e69a76ec874c2b8b466
Victor Sim <victorwtsim@gmail.com>
2021-08-30 14:19:29 +0800

    Formatting

--- v3.2.0/chess_env.py
+++ v3.2.0/chess_env.py
@@ -1,30 +1,34 @@
 import chess
-from board_conversion import *
-from q_network import *
-from rewards import * 
-from mcts import *
-from numpy.random import choice
 from IPython.display import clear_output
+from numpy.random import choice
+
+from board_conversion import *
+from mcts import *
+from q_network import *
+from rewards import *
+
+
 class ChessEnv():
-    __slots__ = ["board","X","y_p","y_v",
-                 "positions","loss_history",
-                 "move_counter","fast_counter",
-                 "pgn","pgns","outcome"]
+    __slots__ = ["board", "X", "y_p", "y_v",
+                 "positions", "loss_history",
+                 "move_counter", "fast_counter",
+                 "pgn", "pgns", "outcome"]
+
     def __init__(self):
         self.board = chess.Board()
         self.X = []
-        self.y_p= []
-        self.y_v= []
+        self.y_p = []
+        self.y_v = []
         self.positions = []
         self.loss_history = []
         self.move_counter = 1
         self.fast_counter = 0
         self.pgn = ''
         self.pgns = []
-        
+
     def translate_board(self):
         return translate_board(self.board)
-    
+
     def reset(self):
         self.board = chess.Board()
         self.pgns.append(self.pgn)
@@ -32,39 +36,37 @@ class ChessEnv():
         self.fast_counter = 0
         self.pgn = ''
         if len(self.pgns) > 100:
-          self.pgns.pop(-1)
+            self.pgns.pop(-1)
         return translate_board(self.board)
 
-    def update_pgn(self,move):
-      if self.fast_counter % 2 == 0:
-          self.pgn += str(self.move_counter)+ '.'
-          self.move_counter += 1
-      self.fast_counter += 1
-      string = str(self.board.san(move))+' '
-      self.pgn+=string
-      
-    
-    def step(self,action):
+    def update_pgn(self, move):
+        if self.fast_counter % 2 == 0:
+            self.pgn += str(self.move_counter) + '.'
+            self.move_counter += 1
+        self.fast_counter += 1
+        string = str(self.board.san(move))+' '
+        self.pgn += string
+
+    def step(self, action):
         self.update_pgn(action)
         self.board.push(action)
-        
 
     def evaluate_reward(self):
         if self.board.is_checkmate():
             return 1
         else:
             return 0
-        
-    def execute_episode(self,model,simulations = 100):
+
+    def execute_episode(self, model, simulations=100):
         episode_y_v = []
         v_replacements = {
-            True : 0,
-            False : 0
+            True: 0,
+            False: 0
         }
         move_counter = 0
-        
+
         while True and move_counter < 160:
-            print('Move:',move_counter)
+            print('Move:', move_counter)
             if self.board.is_game_over():
                 self.outcome = self.board.result()
                 results = np.array(self.outcome.split('-'))
@@ -80,39 +82,40 @@ class ChessEnv():
 
             self.positions.append(self.board.copy())
             self.positions = self.positions[-8:]
-            tree = MonteCarloTree(model,self.board,self.positions)
-            
-            policy = tree.run_simulations(simulations = simulations)
-            self.X.append(generate_input(self.positions)) 
-            data_policy = convert_policy(self.board,policy)
+            tree = MonteCarloTree(model, self.board, self.positions)
+
+            policy = tree.run_simulations(simulations=simulations)
+            self.X.append(generate_input(self.positions))
+            data_policy = convert_policy(self.board, policy)
             self.y_p.append(data_policy)
             episode_y_v.append(self.board.turn)
-            
+
             a = choice(len(policy), p=policy)
-            move = list(self.board.legal_moves)[a]# sample action from improved policy
+            # sample action from improved policy
+            move = list(self.board.legal_moves)[a]
             self.step(move)
-            
+
             del data_policy
             del a
             del move
             del tree
         clear_output()
-        
+
         episode_y_v = [v_replacements[boolean] for boolean in episode_y_v]
         self.y_v += episode_y_v
-        
+
         del episode_y_v
         del v_replacements
         return self.board.result()
-    
-    def train_model(self,q_model,epochs = 100):
+
+    def train_model(self, q_model, epochs=100):
         rep_model = q_model
         print('Training Model...')
-        X = np.array(self.X).reshape(len(self.X),17,8,8,12)
+        X = np.array(self.X).reshape(len(self.X), 17, 8, 8, 12)
         y_p = np.array(self.y_p)
-        y_v = np.array(self.y_v).reshape(len(self.y_v),1)
-        print(X.shape,y_p.shape,y_v.shape)
-        history = rep_model.model.fit(X,[y_p,y_v],epochs = epochs, verbose = 0)
+        y_v = np.array(self.y_v).reshape(len(self.y_v), 1)
+        print(X.shape, y_p.shape, y_v.shape)
+        history = rep_model.model.fit(X, [y_p, y_v], epochs=epochs, verbose=0)
         loss = history.history['loss']
         self.loss_history.append(min(loss))
         return rep_model
--- v3.2.0/mcts.py
+++ v3.2.0/mcts.py
@@ -3,6 +3,7 @@ from board_conversion import *
 # ! Remove The Move Stacks from Each Board to Reduce total number of moves
 c_puct = 10
 
+
 def pos_cont(board):
     boards = []
     legal_moves = list(board.legal_moves)
@@ -12,11 +13,13 @@ def pos_cont(board):
         copy_board.push(move)
         copy_board.move_stack = []
         boards.append(copy_board)
-    return boards,legal_moves
+    return boards, legal_moves
+
 
 class Action():
-    __slots__ = ["N","W","Q","V","state","move_idx","pred_states"]
-    def __init__(self,state,move_idx,parent_nodes):
+    __slots__ = ["N", "W", "Q", "V", "state", "move_idx", "pred_states"]
+
+    def __init__(self, state, move_idx, parent_nodes):
         self.N = 0
         self.W = 0
         self.Q = 0
@@ -24,24 +27,26 @@ class Action():
         self.state = state
         self.state.__dict__['_stack'] = []
         self.move_idx = move_idx
-        
+
         self.pred_states = [self.state]
         for parent_node in parent_nodes:
             self.pred_states.append(parent_node.board)
-            
+
         self.pred_states = self.pred_states[:8]
         # ! IF NOT CONVERGE COULD BE CAUSE! DATA IMBALANCE.
-        
-    def evaluate(self,P,v,Ns):
+
+    def evaluate(self, P, v, Ns):
         P = P[0][self.move_idx]
         self.V = v
-        U = c_puct * P * (np.sqrt(Ns)/(1+ self.N))
+        U = c_puct * P * (np.sqrt(Ns)/(1 + self.N))
         QpU = U + self.Q
         return QpU
 
+
 class Node:
-    __slots__ = ["board","move","child_nodes","parents","states","action"]
-    def __init__(self,board,move,parents):
+    __slots__ = ["board", "move", "child_nodes", "parents", "states", "action"]
+
+    def __init__(self, board, move, parents):
         self.board = board
         self.board.__dict__['_stack'] = []
         if move:
@@ -50,102 +55,106 @@ class Node:
             self.move = None
         self.child_nodes = []
         self.parents = parents
-        self.states = np.append(np.array(parents),self) 
-        self.action = Action(self.board,self.move,self.states)
-            
+        self.states = np.append(np.array(parents), self)
+        self.action = Action(self.board, self.move, self.states)
+
     def extend(self):
         if not(self.child_nodes):
-            continuations,legal_moves = pos_cont(self.board)
+            continuations, legal_moves = pos_cont(self.board)
             self.board.move_stack = []
             new_parents = self.parents
             new_parents.append(self)
             for i in range(len(continuations)):
-                self.child_nodes.append(Node(continuations[i],legal_moves[i],new_parents))
-            
+                self.child_nodes.append(
+                    Node(continuations[i], legal_moves[i], new_parents))
+
             del legal_moves
             del new_parents
-            
+
+
 def evaluate_reward(board):
     if board.is_checkmate():
         return 1
     else:
         return -1
 
+
 class MonteCarloTree():
-    __slots__ = ["prev_node","chain","root_node","model"]
-    def __init__(self,model,board,parents):
-        self.create_root_node(board,parents)
+    __slots__ = ["prev_node", "chain", "root_node", "model"]
+
+    def __init__(self, model, board, parents):
+        self.create_root_node(board, parents)
         self.prev_node = self.root_node
         self.chain = []
         self.model = model
 
-    def create_root_node(self,board,parents):
+    def create_root_node(self, board, parents):
         root_parents = []
         for position in parents:
-            node = Node(position,num2move[0],[])
+            node = Node(position, num2move[0], [])
             root_parents.append(node)
-        root_node = Node(board,num2move[0],root_parents)
+        root_node = Node(board, num2move[0], root_parents)
         del root_parents
         self.root_node = root_node
-        
+
     def simulate(self):
         #print('Simulation Started')
         self.chain.append(self.prev_node)
-        #print(self.prev_node.board)
-        if self.prev_node.board.is_game_over():#
+        # print(self.prev_node.board)
+        if self.prev_node.board.is_game_over():
             #print('Terminal State Reached')
             reward = evaluate_reward(self.prev_node.board)
             for node in self.chain[1:]:
-                node.action.N += 1 
+                node.action.N += 1
                 node.action.W += reward
                 node.action.Q = node.action.W/node.action.N
             self.chain = []
             self.prev_node = self.root_node
             print('termin')
-            return evaluate_reward(self.prev_node.board)   
-        
+            return evaluate_reward(self.prev_node.board)
+
         if not(self.prev_node.child_nodes):
             self.prev_node.extend()
             #print('Leaf Node Reached')
             self.prev_node.action.N += 1
             return -self.prev_node.action.V
-        #? Extend and only happen when not done before
-        
+        # ? Extend and only happen when not done before
+
         QpUs = []
         child_nodes = self.prev_node.child_nodes
         Ns = [child_node.action.N for child_node in child_nodes]
-        P,v = self.model.predict(child_nodes[0].action.pred_states)
+        P, v = self.model.predict(child_nodes[0].action.pred_states)
         P = P.numpy()
         for child_node in child_nodes:
-            QpU = child_node.action.evaluate(P,v,np.sum(Ns)) 
+            QpU = child_node.action.evaluate(P, v, np.sum(Ns))
             QpUs.append(QpU)
-        
-        next_node = child_nodes[np.random.choice(np.where(QpUs == max(QpUs))[0])]
+
+        next_node = child_nodes[np.random.choice(
+            np.where(QpUs == max(QpUs))[0])]
         next_node.action.N += 1
         Ns = [child_node.action.N for child_node in child_nodes]
         self.prev_node = next_node
         #print('Action Calculated')
         v = self.simulate()
-        
+
         for node in self.chain:
-            node.action.Q = (node.action.N*node.action.Q +v)/(node.action.N+1)
+            node.action.Q = (node.action.N*node.action.Q + v)/(node.action.N+1)
             node.action.N += 1
             del node
-            
+
         del Ns
         del QpUs
         del P
         #print('Visits Corrected')
         return -v
-    
-    def run_simulations(self,simulations = 100):   
+
+    def run_simulations(self, simulations=100):
         for _ in range(simulations):
             #print('EPISODE: '+str(_))
             self.simulate()
         first_gen = self.root_node.child_nodes
         Ns = [node.action.N for node in first_gen]
-        print("NS",Ns)
+        print("NS", Ns)
         policy = [N/np.sum(Ns) for N in Ns]
         del Ns
         return policy
-        
